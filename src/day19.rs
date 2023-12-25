132c038939e9c8a565f0e29dd6096d788d0c392e
Darius Makaitis <dm_github@portkullis.com>
2023-12-25 15:42:02 -0600

    Completed part 2 for day 19.

--- src/day19.rs
+++ src/day19.rs
@@ -3,6 +3,7 @@ use crate::day19::Variable::{A, M, S, X};
 use itertools::Itertools;
 use std::collections::HashMap;
 
+#[derive(Debug)]
 enum Variable {
     X,
     M,
@@ -10,11 +11,13 @@ enum Variable {
     S,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 enum Operator {
     Less,
     Greater,
 }
 
+#[derive(Debug)]
 struct Rule<'a> {
     var: Variable,
     op: Operator,
@@ -53,6 +56,219 @@ impl<'a> From<&'a str> for Rule<'a> {
     }
 }
 
+impl<'a> Rule<'a> {
+    fn split_template(&self, template: &Template) -> (Option<Template>, Option<Template>) {
+        let rule = template.rule.clone();
+        let Template {
+            rule: _,
+            x,
+            m,
+            a,
+            s,
+        } = *template;
+
+        match self.var {
+            X => {
+                let n;
+                let r;
+
+                if self.op == Less {
+                    (n, r) = if template.x.1 < self.val {
+                        (None, Some(Template { rule, x, m, a, s }))
+                    } else {
+                        (
+                            Some(Template {
+                                rule: String::from(self.result),
+                                x: (template.x.0, self.val - 1),
+                                m,
+                                a,
+                                s,
+                            }),
+                            Some(Template {
+                                rule,
+                                x: (self.val, template.x.1),
+                                m,
+                                a,
+                                s,
+                            }),
+                        )
+                    };
+                } else {
+                    (n, r) = if template.x.0 > self.val {
+                        (None, Some(Template { rule, x, m, a, s }))
+                    } else {
+                        (
+                            Some(Template {
+                                rule: String::from(self.result),
+                                x: (self.val + 1, template.x.1),
+                                m,
+                                a,
+                                s,
+                            }),
+                            Some(Template {
+                                rule,
+                                x: (template.x.0, self.val),
+                                m,
+                                a,
+                                s,
+                            }),
+                        )
+                    };
+                }
+                return (n, r);
+            }
+            M => {
+                let n;
+                let r;
+
+                if self.op == Less {
+                    (n, r) = if template.m.1 < self.val {
+                        (None, Some(Template { rule, x, m, a, s }))
+                    } else {
+                        (
+                            Some(Template {
+                                rule: String::from(self.result),
+                                x,
+                                m: (template.m.0, self.val - 1),
+                                a,
+                                s,
+                            }),
+                            Some(Template {
+                                rule,
+                                x,
+                                m: (self.val, template.m.1),
+                                a,
+                                s,
+                            }),
+                        )
+                    };
+                } else {
+                    (n, r) = if template.m.0 > self.val {
+                        (None, Some(Template { rule, x, m, a, s }))
+                    } else {
+                        (
+                            Some(Template {
+                                rule: String::from(self.result),
+                                x,
+                                m: (self.val + 1, template.m.1),
+                                a,
+                                s,
+                            }),
+                            Some(Template {
+                                rule,
+                                x,
+                                m: (template.m.0, self.val),
+                                a,
+                                s,
+                            }),
+                        )
+                    };
+                }
+                return (n, r);
+            }
+            A => {
+                let n;
+                let r;
+
+                if self.op == Less {
+                    (n, r) = if template.a.1 < self.val {
+                        (None, Some(Template { rule, x, m, a, s }))
+                    } else {
+                        (
+                            Some(Template {
+                                rule: String::from(self.result),
+                                x,
+                                m,
+                                a: (template.a.0, self.val - 1),
+                                s,
+                            }),
+                            Some(Template {
+                                rule,
+                                x,
+                                m,
+                                a: (self.val, template.a.1),
+                                s,
+                            }),
+                        )
+                    };
+                } else {
+                    (n, r) = if template.a.0 > self.val {
+                        (None, Some(Template { rule, x, m, a, s }))
+                    } else {
+                        (
+                            Some(Template {
+                                rule: String::from(self.result),
+                                x,
+                                m,
+                                a: (self.val + 1, template.a.1),
+                                s,
+                            }),
+                            Some(Template {
+                                rule,
+                                x,
+                                m,
+                                a: (template.a.0, self.val),
+                                s,
+                            }),
+                        )
+                    };
+                }
+                return (n, r);
+            }
+            S => {
+                let n;
+                let r;
+
+                if self.op == Less {
+                    (n, r) = if template.s.1 < self.val {
+                        (None, Some(Template { rule, x, m, a, s }))
+                    } else {
+                        (
+                            Some(Template {
+                                rule: String::from(self.result),
+                                x,
+                                m,
+                                a,
+                                s: (template.s.0, self.val - 1),
+                            }),
+                            Some(Template {
+                                rule,
+                                x,
+                                m,
+                                a,
+                                s: (self.val, template.s.1),
+                            }),
+                        )
+                    };
+                } else {
+                    (n, r) = if template.s.0 > self.val {
+                        (None, Some(Template { rule, x, m, a, s }))
+                    } else {
+                        (
+                            Some(Template {
+                                rule: String::from(self.result),
+                                x,
+                                m,
+                                a,
+                                s: (self.val + 1, template.s.1),
+                            }),
+                            Some(Template {
+                                rule,
+                                x,
+                                m,
+                                a,
+                                s: (template.s.0, self.val),
+                            }),
+                        )
+                    };
+                }
+                return (n, r);
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Workflow<'a> {
     rules: Vec<Rule<'a>>,
     default: &'a str,
@@ -94,6 +310,28 @@ impl<'a> Workflow<'a> {
         }
 
         return self.default;
+    }
+
+    fn split_template(&self, template: &Template) -> Vec<Template> {
+        let mut t = template.clone();
+        let mut result = vec![];
+
+        for rule in &self.rules {
+            let (a, b) = rule.split_template(&t);
+            if let Some(a) = a {
+                result.push(a);
+            }
+
+            if let Some(b) = b {
+                t = b.clone();
+            } else {
+                break;
+            }
+        }
+        t.rule = String::from(self.default);
+        result.push(t);
+
+        result
     }
 }
 
@@ -148,6 +386,36 @@ impl From<&str> for Part {
     }
 }
 
+#[derive(Debug, Eq, PartialEq, Clone)]
+struct Template {
+    rule: String,
+    x: (u32, u32),
+    m: (u32, u32),
+    a: (u32, u32),
+    s: (u32, u32),
+}
+
+impl Template {
+    fn new() -> Self {
+        Template {
+            rule: String::from("in"),
+            x: (1, 4000),
+            m: (1, 4000),
+            a: (1, 4000),
+            s: (1, 4000),
+        }
+    }
+
+    fn get_distinct_count(&self) -> u64 {
+        let x = (self.x.1 + 1 - self.x.0) as u64;
+        let m = (self.m.1 + 1 - self.m.0) as u64;
+        let a = (self.a.1 + 1 - self.a.0) as u64;
+        let s = (self.s.1 + 1 - self.s.0) as u64;
+
+        x * m * a * s
+    }
+}
+
 fn parse_workflow(s: &str) -> (&str, Workflow) {
     let (name, rules) = s
         .strip_suffix("}")
@@ -168,20 +436,16 @@ fn parse_workflow(s: &str) -> (&str, Workflow) {
 /// 'input' - The input.
 pub fn part_one(input: &str) -> u32 {
     let (workflows, parts) = input.split("\n\n").collect_tuple().unwrap();
-
     let workflows: HashMap<&str, Workflow> = workflows.lines().map(|w| parse_workflow(w)).collect();
-
     let parts = parts.lines().map(|line| Part::from(line)).collect_vec();
 
     let mut accepted = vec![];
 
     for part in &parts {
-        // println!("Checking new part...");
         let mut cur = "in";
 
         loop {
             let result = workflows[cur].get_result(part);
-            // println!("    {cur} => {result}");
             if result == "A" {
                 accepted.push(part);
                 break;
@@ -193,8 +457,6 @@ pub fn part_one(input: &str) -> u32 {
         }
     }
 
-    // println!("Accepted part count: {}", accepted.len());
-
     accepted.iter().map(|p| p.x + p.m + p.a + p.s).sum()
 }
 
@@ -204,13 +466,139 @@ pub fn part_one(input: &str) -> u32 {
 ///
 /// 'input' - The input.
 pub fn part_two(input: &str) -> u64 {
-    0
+    let (workflows, _) = input.split("\n\n").collect_tuple().unwrap();
+    let workflows: HashMap<&str, Workflow> = workflows.lines().map(|w| parse_workflow(w)).collect();
+
+    let mut templates = vec![Template::new()];
+    let mut accepted = vec![];
+
+    while let Some(template) = templates.pop() {
+        println!("{:?}", &template);
+        let rule = &workflows[template.rule.as_str()];
+        println!("    {:?}", rule);
+        let new_templates = rule.split_template(&template);
+
+        for t in new_templates {
+            println!("    {:?}", t);
+            if t.rule == "A" {
+                accepted.push(t);
+            } else if t.rule != "R" {
+                templates.push(t);
+            }
+        }
+    }
+
+    println!("{} accepted templates", accepted.len());
+
+    accepted.sort_by_key(|p| p.s.0);
+    accepted.sort_by_key(|p| p.a.0);
+    accepted.sort_by_key(|p| p.m.0);
+    accepted.sort_by_key(|p| p.x.0);
+    for a in &accepted {
+        println!(
+            "Accepted {:?} with {} combinations",
+            a,
+            a.get_distinct_count()
+        );
+    }
+
+    accepted.iter().map(|a| a.get_distinct_count()).sum()
 }
 
 #[cfg(test)]
 mod tests {
     use crate::day19::*;
 
+    #[test]
+    fn test_split_template_on_rule() {
+        let template = Template::new();
+        let rule = Rule {
+            var: X,
+            op: Less,
+            val: 1000,
+            result: "a",
+        };
+        let (a, b) = rule.split_template(&template);
+
+        assert_eq!(
+            a,
+            Some(Template {
+                rule: String::from("a"),
+                x: (1, 999),
+                m: (1, 4000),
+                a: (1, 4000),
+                s: (1, 4000)
+            })
+        );
+        assert_eq!(
+            b,
+            Some(Template {
+                rule: String::from("in"),
+                x: (1000, 4000),
+                m: (1, 4000),
+                a: (1, 4000),
+                s: (1, 4000)
+            })
+        );
+
+        let rule = Rule {
+            var: X,
+            op: Greater,
+            val: 1000,
+            result: "a",
+        };
+        let (a, b) = rule.split_template(&template);
+
+        assert_eq!(
+            a,
+            Some(Template {
+                rule: String::from("a"),
+                x: (1001, 4000),
+                m: (1, 4000),
+                a: (1, 4000),
+                s: (1, 4000)
+            })
+        );
+        assert_eq!(
+            b,
+            Some(Template {
+                rule: String::from("in"),
+                x: (1, 1000),
+                m: (1, 4000),
+                a: (1, 4000),
+                s: (1, 4000)
+            })
+        );
+
+        let rule = Rule {
+            var: M,
+            op: Less,
+            val: 1000,
+            result: "a",
+        };
+        let (a, b) = rule.split_template(&template);
+
+        assert_eq!(
+            a,
+            Some(Template {
+                rule: String::from("a"),
+                x: (1, 4000),
+                m: (1, 999),
+                a: (1, 4000),
+                s: (1, 4000)
+            })
+        );
+        assert_eq!(
+            b,
+            Some(Template {
+                rule: String::from("in"),
+                x: (1, 4000),
+                m: (1000, 4000),
+                a: (1, 4000),
+                s: (1, 4000)
+            })
+        );
+    }
     #[test]
     fn part_one_correct() {
         assert_eq!(
@@ -237,7 +625,7 @@ hdj{m>838:A,pv}
         );
     }
 
-    // #[test]
+    #[test]
     fn part_two_correct() {
         assert_eq!(
             part_two(
