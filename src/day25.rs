1900cfb43a5f60d5d8422f386d3f152fb1d5a620
Darius Makaitis <dm_github@portkullis.com>
2024-01-01 09:28:54 -0600

    Added comments to day 25 so I'll remember how this solution works when I look at it again in the future.

--- src/day25.rs
+++ src/day25.rs
@@ -10,6 +10,8 @@ struct Edge {
 
 impl Edge {
     fn new(a: usize, b: usize) -> Self {
+        // Ensures nodes for an edge are always in a consistent order so when we place them into a set while we're
+        // parsing the input, we're guaranteed not to get duplicate entries...
         if a < b {
             Edge { a, b }
         } else {
@@ -32,12 +34,15 @@ impl From<&str> for Graph {
 
             let ends = ends.split_whitespace().collect_vec();
 
+            // As we encounter each node name for the first time, add it to a hash map so we can assign it a unique
+            // integer value that we can easily look up the next time we encounter the same name...
             if !names.contains_key(start) {
                 names.insert(start, names.len());
             }
             let a = *names.get(start).unwrap();
 
             for end in ends {
+                // Again, assign a unique integer value for each new node name we encounter...
                 if !names.contains_key(end) {
                     names.insert(end, names.len());
                 }
@@ -53,18 +58,31 @@ impl From<&str> for Graph {
 
 impl Graph {
     fn minimum_cut(&self) -> (usize, usize) {
+        // This is an implementation of single iteration of Karger's algorithm.
+
+        // Since we assigned consecutive integer values to each node in the graph while parsing, the
+        // number of nodes in the graph is equal to the highest node ID plus one
         let mut num_nodes = self.edges.iter().map(|e| e.b + 1).max().unwrap();
 
+        // As we collapse the graph, we'll keep track of the number of initial nodes that have been merged into any
+        // remaining nodes. We'll use this to calculate the puzzle output at the end when only two nodes remain.
+        // The initial value for each node is 1 since at the beginning, each node only represents itself.
         let mut counts = vec![1usize; num_nodes];
 
         let mut edges = self.edges.iter().copied().collect_vec();
 
+        // We'll continue to collapse edges until only two nodes remain.
         while num_nodes > 2 {
+            // Pick a random edge to collapse and get the two node ID's for the edge.
+            // (Karger's algorithm works because there are far more edges NOT in the minimal cut, so odds are we'll
+            // select an edge not on the minimal cut)
             let r = random::<usize>() % edges.len();
             let edge = edges.get(r).unwrap();
             let a = edge.a;
             let b = edge.b;
 
+            // Go through all the edges and replace any references to node B with node A since we are merging the two
+            // nodes together and only need to keep one.
             edges.iter_mut().for_each(|edge| {
                 if edge.a == b {
                     edge.a = a;
@@ -73,17 +91,27 @@ impl Graph {
                     edge.b = a;
                 };
             });
+            // Remove any edges where both nodes are the same - those are the edges we just collapsed
+            // (there may be more than one since as we continue to collapse the graph, we may end up with multiple
+            // edges connecting any two nodes)
             edges.retain(|e| e.a != e.b);
 
+            // Transfer the node counts from node B into node A since we just merged them both together.
             counts[a] += counts[b];
             counts[b] = 0;
 
             num_nodes -= 1;
         }
 
+        // With only two nodes left, all the remaining edges must connect those two nodes, so we can get the two
+        // node ID's from any remaining edge (like the first one).
+
+        // Get the total counts for the two remaining nodes:
         let a = counts[edges.first().unwrap().a];
         let b = counts[edges.first().unwrap().b];
 
+        // Return the number of edges that remain, along with the product of how many nodes had been merged into either
+        // end of those edges:
         (edges.len(), a * b)
     }
 }
@@ -97,13 +125,19 @@ pub fn part_one(input: &str) -> usize {
     let g = Graph::from(input);
 
     let mut size = usize::MAX;
-    let mut sum = 0;
+    let mut product = 0;
 
+    // Theoretically, we can find the minimal cut of the graph by iterating through Karger's algorithm N * log(N)
+    // times (where N is the number of nodes in the graph) and keeping the the result with the lowest number of
+    // remaining edges. However, we know from the puzzle description that the minimal cut will have three edges, so
+    // we only have to keep repeating until we find a solution that collapses into only three edges. This saves us from
+    // having to do approximately 15,000 iterations when in practice we can find a solution in only a few hundred
+    // iterations...
     while size != 3 {
-        (size, sum) = g.minimum_cut();
+        (size, product) = g.minimum_cut();
     }
 
-    sum
+    product
 }
 
 ///
